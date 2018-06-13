ac4f8af45af232adf15451d64f5acb38947db01c
Brianna <brianna@Briannas-MacBook-Air.local>
2018-06-13 17:25:53 -0400

    BOS21 code through

---
+++ lib/soundtrack.rb
@@ -0,0 +1,21 @@
+class Soundtrack
+  attr_reader :songs
+
+  def initialize(name, songs)
+    @name = name
+    @songs = songs
+  end
+
+  def add_song(song)
+    @songs << song
+  end
+
+  def print_soundtrack
+    output = ''
+    @songs.each do |song|
+      # output += song.name + "\n"
+      output += "#{song.name}\n"
+    end
+    output
+  end
+end
